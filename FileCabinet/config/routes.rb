8766b025419520a0f9f66d5392d67f4736d3aba9
Leland Krych <leland.krych@gmail.com>
2017-04-20 04:12:22 +0000

    change splash page for authenticated user

--- FileCabinet/config/routes.rb
+++ FileCabinet/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
   get 'welcome/index'
 
   root 'welcome#index'
   
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
   devise_for :users
 end
